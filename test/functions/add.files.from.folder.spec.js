623184c26d34f5288d689b9d91034d25cca16532
cope <predrag@stojadinovic.net>
2025-07-17 16:37:30 +0200

    chore: upgrading packages

---
+++ test/functions/add.files.from.folder.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import {expect} from 'chai';
+
+describe('add.files.from.folder tests', () => {
+	expect(true).to.be.true;
+	// TODO: implement tests
+	it('should be implemented');
+});
---
+++ test/functions/list.subfolders.files.by.folder.name.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import {expect} from 'chai';
+
+describe('list.subfolders.files.by.folder.name tests', () => {
+	expect(true).to.be.true;
+	// TODO: implement tests
+	it('should be implemented');
+});
